107be2acee6f5e86a138506c3b33db209eea1710
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2024-09-06 19:36:59 -0400

    Code quiz Button Disable on Checkbox Check

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,9 +1,19 @@
+import "./App.css"
+import React from "react";
 function App() {
-  return (
-    <div>
-      <h1>I'm gonna learn React Testing Library</h1>
-    </div>
-  );
+    const [buttonColor, setButtonColor] = React.useState("red")
+    const nextColor = buttonColor === "red" ? "blue" : "red"
+    const [disabled, setDisabled] = React.useState(false)
+
+
+    return (
+        <div>
+            <button disabled={disabled} className={buttonColor} onClick={() => setButtonColor(nextColor)}>Change to {nextColor}</button>
+            <br/>
+            <input onChange={e => setDisabled(e.target.checked)} type="checkbox" id="disable-button-checkbox" defaultChecked={disabled}/>
+            <label htmlFor="disable-button-checkbox">Disable button</label>
+        </div>
+    );
 }
 
 export default App;
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import {logRoles, render, screen, fireEvent} from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+test("button click flow", () => {
+    // render App
+    render(<App/>);
+    // logRoles(container)
+    // find the button
+    const buttonElement = screen.getByRole("button", {name: /blue/i});
+
+    // check initial color
+    expect(buttonElement).toHaveClass("red")
+
+    // click the button
+    fireEvent.click(buttonElement);
+
+    // check button text
+    expect(buttonElement).toHaveTextContent(/red/i)
+
+
+    // check the button color
+    expect(buttonElement).toHaveClass("blue")
+    expect(buttonElement).toHaveStyle({"background-color": "rgb(0,0,255)"})
 });
+
+test("checkbox flow", () => {
+    render(<App/>);
+    // find elements
+    const buttonElement = screen.getByRole("button", {name: /blue/i});
+    const checkboxElement = screen.getByRole("checkbox", {name: /disable button/i});
+
+    // check initial condition
+    expect(buttonElement).toBeEnabled();
+    expect(checkboxElement).not.toBeChecked();
+
+    fireEvent.click(checkboxElement);
+    expect(buttonElement).toBeDisabled();
+
+    fireEvent.click(checkboxElement);
+    expect(buttonElement).toBeEnabled();
+
+})
